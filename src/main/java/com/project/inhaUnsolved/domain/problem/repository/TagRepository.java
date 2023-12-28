dfe4559442fd4a611f78ec82bf60ea9c207ca98c
Lieslot <rlaxodud1999@gmail.com>
2023-12-28 12:37:40 +0900

    feat(problem): TagRepository 추가

---
+++ src/main/java/com/project/inhaUnsolved/domain/problem/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package com.project.inhaUnsolved.domain.problem.repository;
+
+import com.project.inhaUnsolved.domain.problem.Tag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+}
