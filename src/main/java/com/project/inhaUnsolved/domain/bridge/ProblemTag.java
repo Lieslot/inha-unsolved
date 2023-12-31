059409d8c596988ca4a5f868b0b8a4d4672768a1
Lieslot <rlaxodud1999@gmail.com>
2023-12-31 12:55:33 +0900

    feat(bridge): ProblemTag 연결테이블의 복합키 식별자 추가

--- src/main/java/com/project/inhaUnsolved/domain/bridge/ProblemTag.java
+++ src/main/java/com/project/inhaUnsolved/domain/bridge/ProblemTag.java
@@ -2,6 +2,8 @@ package com.project.inhaUnsolved.domain.bridge;
 
 import com.project.inhaUnsolved.domain.problem.domain.UnsolvedProblem;
 import com.project.inhaUnsolved.domain.problem.domain.Tag;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -9,23 +11,28 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapKey;
+import jakarta.persistence.MapsId;
 import lombok.Getter;
 
 @Getter
 @Entity
 public class ProblemTag {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    @EmbeddedId
+    private ProblemTagId id;
 
+    @MapsId("tagId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "tag_id")
     private Tag tag;
 
+    @MapsId("problemId")
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "problem_id")
     private UnsolvedProblem problem;
 
 
 }
+
+
